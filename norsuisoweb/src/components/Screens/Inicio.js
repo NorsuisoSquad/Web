49c6115525703b8e47bc5784c52d0fa5010b0af3
Emmanuel Trujillo Castillo <emmanuel1998@hotmail.com>
2021-04-13 04:38:54 -0700

    Ajustes a Inicio (borre comentarios innecesarios)

--- norsuisoweb/src/components/Screens/Inicio.js
+++ norsuisoweb/src/components/Screens/Inicio.js
@@ -4,18 +4,11 @@ import { Typography, Button } from '@material-ui/core';
 
 /*Borrar desde aqui---------------------------------------------------------------------------*/
 import { Box } from '@material-ui/core';
-import MainTabs from '../Layout/MainTabs';
-import Logo from '../Layout/Logo';
 /*Hasta aca-----------------------------------------------------------------------------------*/
 
-import { TwitchEmbed, TwitchChat, TwitchClip, TwitchPlayer } from 'react-twitch-embed';
+import { TwitchChat, TwitchPlayer } from 'react-twitch-embed';
 
 const useStyles = makeStyles((theme) => ({
-    /*    borde: {
-            padding:'0px',
-            margin:'0px',
-            backgroundColor:'white'
-        },*/
     otro: {
         height: 'auto'
     }
@@ -24,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
 const Inicio = () => {
     const classes = useStyles();
     return (
-        <div className={/*classes.borde, */classes.otro}>
+        <div className={classes.otro}>
             Pag de Inicio
 
             <hr />
@@ -37,17 +30,6 @@ const Inicio = () => {
                 <TwitchChat channel="emmanueltc" width="325px" height="350px" theme='dark' />
             </Box>
 
-
-            {/*Borrar desde aqui---------------------------------------------------------------------------*/}
-            {/*            <Box display="flex" alignItems="flex-start" p={1} m={1} bgcolor="background.paper" css={{ height: 50 }}>
-                <Box flexGrow={1}><Typography variant="h6">NorsuisoSquad</Typography></Box>
-                <Box flexGrow={1}><MainTabs className={classes.tabs} /></Box>
-                <Box flexGrow={1} display="flex">
-                    <Box flexGrow={1}></Box>
-                    <Box><Logo float='right' width={50} className={classes.logo} /></Box>
-                </Box>
-            </Box>*/}
-            {/*Hasta aca-----------------------------------------------------------------------------------*/}
 
             {/*<Box display="flex">
                 <Box flexGrow={1}>
